bc0ef4e83c369e94a97f3ed3f49c393981d29580
kaylynyuh <yuhkay4@gmail.com>
2016-07-31 09:52:16 -0700

    ghost code deleted

--- scripts/model/model.js
+++ scripts/model/model.js
@@ -1,5 +1,3 @@
-// var baseUrl = 'https://ridb.recreation.gov/api/v1/';
-// var authParam = '?apikey=300070357BB44A5E840841EEB4552CE8';
 var baseUrl = 'https://trailapi-trailapi.p.mashape.com/';
 var authKey = 'KLpWSysVJTmshDzPyETfAf7t3KLhp1enYfsjsnb8fX4wPIx2Wd';
 var acceptType = 'text/plain';
@@ -19,12 +17,10 @@ function lookupPlace(lat, lng){
       //console.log(data);
     }
   }).done(function(data){
-    // pass
   });
 }
 
 function getPlace(data) {
-  // loop places array
   for (var i = 0; i < data.places.length; i++) {
     getPlaceInfo(data.places[i]);
   }
@@ -37,26 +33,8 @@ var getPlaceInfo = function (currentPlace) {
     var activity_type = currentActivity.activity_type_name;
     var description = currentActivity.description;
     var activityLength = currentActivity['length']; // use bracket b/c 'length' is reserved
-    //console.log(activityLength);
     console.log(description);
-    //console.log(activity_type);
-    //console.log(currentActivity);
   }
-  //console.log(currentPlace);
-  /*
-  var place = {
-    directions: currentPlace.directions,
-    location: {
-      lat: currentPlace.lat,
-      long: currentPlace.lon
-    }
-    // activities: currentPlace.activities.map(function(currentActivity) {
-    //   getActivities(currentActivity);
-    // };
-  };
-  console.log(place);
-  placeholder.push(place);
-  */
 };
 
 var getActivities = function (currentActivity) {
