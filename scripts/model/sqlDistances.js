f8fe7ba14870e13b6c39710102c2dca061d33192
Erica Winberry <elaewin@gmail.com>
2016-08-01 11:36:52 -0700

    add method to calculate distances from zip location.

--- scripts/model/sqlDistances.js
+++ scripts/model/sqlDistances.js
@@ -3,25 +3,29 @@
 
   //sqlDB.displayHikes = []; //array used to render hikes to the DOM
 
-  sqlDistances.createTable = function(callback) {
+  sqlDistances.createTable = function(callback, zipCode) {
     sqlDistances.deleteEverything();
     webDB.execute(
       'CREATE TABLE IF NOT EXISTS distanceDB (' +
       'id INTEGER PRIMARY KEY, ' +
       'distance FLOAT);'
     );
-    callback();
+    callback(zipCode);
   };
 
-  sqlDistances.insertRecord = function() {
-
-    modelHikes.hikesArray.forEach( function(element) {
-      var newDistance
+  sqlDistances.insertRecord = function(zipCode) {
+    var zipLocation = modelHikes.getLatLng(zipCode);
+    modelHikes.hikesArray.forEach(function(element) {
+      var lat1 = element['location'].lat;
+      var lon1 = element['location'].lon;
+      var lat2 = zipLocation[0];
+      var lon2 = zipLocation[1];
+      var newDistance = distanceCalc.getDistance(lat1, lon1, lat2, lon2);
       webDB.execute(
         [
           {
             'sql': 'INSERT INTO distanceDB (distance) VALUES (?);',
-            'data': [element.distance] // FIX THIS
+            'data': [newDistance]
           }
         ]
       );
