2a9eb8f6d4bac08f4631d75789ab028ec6e146e9
Erica Winberry <elaewin@gmail.com>
2016-08-03 16:44:39 -0700

    add loadHikeResults function.

--- scripts/view/resultsView.js
+++ scripts/view/resultsView.js
@@ -1,7 +1,42 @@
 (function(module) {
   var resultsView = {};
 
-  var resultsCompiler = Handlebars.compile($('#results-template').text());
+  function Hike (opts) {
+    Object.keys(opts).forEach(function(e, index, keys) {
+      this[e] = opts[e];
+    },this);
+  };
+
+  resultsView.resultsArray = [];
+
+  resultsView.loadHikeResults = function () {
+    webDB.execute('SELECT * FROM resultsDB SORT BY distanceFromUser ASC',
+    function(rows) {
+      resultsView.resultsArray = rows.map(function(row) {
+        return new Hike(row);
+      });
+    })
+  };
+
+  resultsView.renderResults = function() {
+    var resultsCompiler = Handlebars.compile($('#results-template').text());
+    //
+  };
+        var actArray = row.activities.split(',');
+        // var sceneArray = row.scenery.split(',');
+        return template(
+          {
+            name: nameOfPlace,
+            hikeDescription: description,
+            activitesSelected: actArray,
+            // scenerySelected: sceneArray,
+            length: lengthOfHike,
+            distance: distanceFromUser
+          }
+        );
+      });
+    });
+  };
 
   resultsView.Run = function() {
     async.series([
