4f60eb89156623120236f0522be4dcf930f03576
kaylynyuh <yuhkay4@gmail.com>
2016-08-04 14:03:36 -0700

    more rerouting

--- scripts/controller/resultsController.js
+++ scripts/controller/resultsController.js
@@ -2,14 +2,9 @@
 
   var resultsController = {};
 
-  resultsController.Run = function() {
+  resultsController.render = function() {
     console.log('resultsController.Run is running.');
     resultsController.updateResultsDB();
-  };
-
-  resultsController.render = function() {
-    console.log('resultsController.render is running.');
-    resultsController.Run();
   };
 
   module.resultsController = resultsController;
