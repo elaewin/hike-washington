869b7bfe7a494209035a978d7dcb5fbbb6b869b9
Erica Winberry <elaewin@gmail.com>
2016-08-05 00:24:31 -0700

    hover text appearing for all current icons (easy to add more). Falling over now.

--- scripts/view/filtersView.js
+++ scripts/view/filtersView.js
@@ -11,35 +11,38 @@
     $('#filters').append('<div id="distance" data-category="distance"></div>');
     $('#distance').append('<h2>Distance</h2>');
     var distancesClass = [
-      { 'color': 'green', 'miles': '0,3' }, //add alt text here
-      { 'color': 'yellow', 'miles': '4,7' },
-      { 'color': 'orange', 'miles': '8,11' },
-      { 'color': 'red', 'miles': '12,800' }
+      { 'color': 'green', 'miles': '0,3', 'title': 'Short hike; less than 3 miles long.' }, //add alt text here
+      { 'color': 'yellow', 'miles': '4,7', 'title': 'Medium length hike; between 3 and 7 miles long.' },
+      { 'color': 'orange', 'miles': '8,11', 'title': 'Long hike; between 7 and 11 miles long.' },
+      { 'color': 'red', 'miles': '12,800', 'title': 'Very long/overnight hike; more than 11 miles long.' }
     ];
+    $('#distance').append('<p>How long you want your hike to be, from short (green), to crazy(red).</p>');
     $('#distance').append('<ul></ul>');
     distancesClass.forEach(function(element){
-      $('div[data-category="distance"] ul').append('<li class="flaticon-hiking ' + element.color + '" value="' + element.miles + '"></li>'); //add alt + distancesClass
+      $('div[data-category="distance"] ul').append('<li class="flaticon-hiking ' + element.color + '" value="' + element.miles + '" title="' + element.title + '"></li>'); //add alt + distancesClass
     });
   };
 
   filtersView.loadActivityFilters = function() {
     $('#filters').append('<div id="activity" data-category="activity"></div>');
     $('div#activity').append('<h2>Other Activities</h2>');
-    var distancesClass = [['flaticon-bicycle-rider','biking'], ['flaticon-night-camping', 'camping'], ['flaticon-walking-with-snowshoes', 'snow']];
+    var distancesClass = [['flaticon-bicycle-rider','biking', 'Mountain Biking'], ['flaticon-night-camping', 'camping', 'Camping'], ['flaticon-walking-with-snowshoes', 'snow', 'Snow Shoeing']];
+    $('div#activity').append('<p>What would you like to do, besides hiking?</p>');
     $('div#activity').append('<ul></ul>');
     distancesClass.forEach(function(element){
-      $('div[data-category="activity"] ul').append('<li class="' + element[0] + ' ltblue other-activity" value="' + element[1] + '"></li>');
+      $('div[data-category="activity"] ul').append('<li class="' + element[0] + ' ltblue other-activity" value="' + element[1] + '" title="' + element[2] + '"></li>');
     });
   };
 
   filtersView.loadSceneryFilters = function() {
     $('#filters').append('<div id="scenery" data-category="scenery"></div>');
     $('#scenery').append('<h2>Scenery</h2>');
-    var distancesClass = [['flaticon-wild-deer', 'wildlife'], ['flaticon-two-pines', 'forrest'], ['flaticon-sun-and-lake', 'water'], ['flaticon-snowed-mountains', 'mountains']];
+    $('#scenery').append('<p>Is there a particular kind of scenery you\'re looking for?</p>');
+    var distancesClass = [['flaticon-wild-deer', 'wildlife', 'Wildlife'], ['flaticon-two-pines', 'forrest', 'Forests'], ['flaticon-sun-and-lake', 'water', 'Lake, River, or Ocean'], ['flaticon-snowed-mountains', 'mountains', 'Mountains']];
     $('#scenery').append('<ul></ul>');
 
     distancesClass.forEach(function(element){
-      $('div[data-category="scenery"] ul').append('<li class="' + element[0] + ' dkgreen other-scenery" value="' + element[1] + '"></li>');
+      $('div[data-category="scenery"] ul').append('<li class="' + element[0] + ' dkgreen other-scenery" value="' + element[1] + '" title="' + element[2] + '"></li>');
     });
   };
 
